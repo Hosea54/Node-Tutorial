c533ccf1e18cf5a91c294c7e7b54c3b378d977c4
Hosea54 <ejohhoseamwongwe@gmail.com>
2025-10-14 20:08:09 -0400

    Globals in Node

---
+++ 2- Globals.js
@@ -0,0 +1,8 @@
+//Globals -No window
+
+//__dirname -path to current directory
+//__filename -file name
+//require  - functions to use modules(commonJs)
+//module - info about current module (file)
+//process - info about env where the program is being executed
+console.log(__dirname);
